97af3a8afe76608f48f67fe1b748f1d6b6a40720
ejimnkonye1 <143567665+ejimnkonye1@users.noreply.github.com>
2025-01-20 20:22:17 +0100

    started this project

---
+++ src/pages/Home.jsx
@@ -0,0 +1,10 @@
+
+
+export const Home = () => {
+    return(
+        <div className="bg-gray-400 h-screen">
+            <p className="text-blue-400 text-center">Tailwind CSs</p>
+            <p>STarted Lost and Found website</p>
+        </div>
+    )
+}

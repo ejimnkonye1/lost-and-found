b96ae8a79ef071fb821839c40f0ccff9954b5fee
ejimnkonye1 <143567665+ejimnkonye1@users.noreply.github.com>
2025-02-03 01:25:26 +0100

    register api works

--- Backend/app/routes/items.py
+++ Backend/app/routes/items.py
@@ -14,25 +14,31 @@ post_collection = db["post_collection"]
 async def Post_item(request:Request):
     data = await request.json()
     username =  data.get('username').lower()
-    name =   data.get('name')
+    email = data.get('email')
+    report_title =   data.get('report_title')
+    report_type =   data.get('report_type')
     description =  data.get('description')
     location_found =  data.get('location_found')
     image =  data.get('image')
+    date = data.get('date')
     whatsapp_number =  data.get('whatsapp_number')
     phone_number =  data.get('phone_number')
-    poster =  await users.find_one({'username':username})
+    poster =  await users.find_one({'username':username, })
 
     if not poster:
         raise HTTPException(status_code=400, detail='invalid sender')
     
     newpost = {
             'poster_name': username,
-            'name': name,
+            'report_title': report_title,
+             'report_type': report_type,
             'description': description,
             'location_found': location_found,
             'image': image,
             'whatsapp_number': whatsapp_number,
             'phone_number': phone_number,
+             'date': date,
+             'email':  email,
             'timestamp': datetime.datetime.now()
         }
 
--- Backend/run.py
+++ Backend/run.py
@@ -1,12 +1,19 @@
 from app import db
 from fastapi import FastAPI,Depends
+from fastapi.middleware.cors import CORSMiddleware
 import uvicorn
 app = FastAPI()
 from app.routes.items import router as item_router
 from app.routes.user import router as users_router
 from app.db import ping_db
 import asyncio
-
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5177"],  # React frontend
+    allow_credentials=True,
+    allow_methods=["*"],  # Allow all HTTP methods (GET, POST, etc.)
+    allow_headers=["*"],  # Allow all headers
+)
 # app.include_router(item_router, prefix='/api/item')
 app.include_router(item_router, prefix='/api')
 app.include_router(users_router, prefix="/api/users")
@@ -28,4 +35,4 @@ for route in app.routes:
     print(route.path, route.methods)
 
 if __name__ == "__main__":
-  uvicorn.run("run:app", host="0.0.0.0", port=8000, reload=True )
+  uvicorn.run("run:app", host="0.0.0.0", port=8000, reload=True ,allow_origins=["http://localhost:5177"])
